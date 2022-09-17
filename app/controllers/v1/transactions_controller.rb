c3e10c7354896138bd80372c082e8829a6ef3f50
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 02:05:13 +0100

    Add transaction route and controller

---
+++ app/controllers/v1/transactions_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module V1
+  class TransactionsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def post
+    end
+
+    def update
+    end
+
+    def delete
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  namespace :v1 do
+    resources :transactions
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "v1/transactions#index"
 end
