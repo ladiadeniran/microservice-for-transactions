286f3c333340062f6848c20dc3dd8942a766dfe1
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 02:04:57 +0100

    Add basic validation to model attributes

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Currency < ApplicationRecord
+  validates :name, :symbol, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,8 @@
 
 class Transaction < ApplicationRecord
   belongs_to :customer
-  belongs_to :input_currency, foreign_key: :input_currency_id, class: 'Currency'
-  belongs_to :output_currency, foreign_key: :input_currency_id, class: 'Currency'
+  belongs_to :input_currency, foreign_key: :input_currency_id, class_name: 'Currency'
+  belongs_to :output_currency, foreign_key: :input_currency_id, class_name: 'Currency'
+
+  validates :customer_id, :input_currency_id, :input_amount, :output_amount, :output_currency_id, :transaction_date, presence: true
 end
