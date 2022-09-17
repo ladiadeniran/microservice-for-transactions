087505f8412936934fd0847f7d32faf94bf8768a
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 03:19:52 +0100

    Add code to currencies

--- db/migrate/20220917002210_create_currencies.rb
+++ db/migrate/20220917002210_create_currencies.rb
@@ -5,10 +5,12 @@ class CreateCurrencies < ActiveRecord::Migration[7.0]
     create_table :currencies, id: :uuid do |t|
       t.string :symbol, null: false
       t.string :name, null: false
+      t.string :code, null: false
 
       t.timestamps
     end
     add_index :currencies, :symbol, unique: true
     add_index :currencies, :name, unique: true
+    add_index :currencies, :code, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_002618) do
   create_table "currencies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "symbol", null: false
     t.string "name", null: false
+    t.string "code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_currencies_on_code", unique: true
     t.index ["name"], name: "index_currencies_on_name", unique: true
     t.index ["symbol"], name: "index_currencies_on_symbol", unique: true
   end
