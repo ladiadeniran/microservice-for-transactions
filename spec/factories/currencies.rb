7dec32cd079a37c2d1814130ccafae750e83935f
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 03:19:33 +0100

    Improve factories to prevent collision - Add sequence to randomize values

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -2,7 +2,8 @@
 
 FactoryBot.define do
   factory :currency do
-    symbol { "$" }
-    name { "dollar" }
+    sequence(:symbol) { |n| Faker::Currency.symbol + "#{n}"}
+    sequence(:name) { |n| Faker::Currency.name + "#{n}" }
+    sequence(:code) { |n| Faker::Currency.code + "#{n}" }
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :transaction do
     association(:customer)
     input_amount { 1.5 }
-    input_currency { nil }
+    association(:input_currency, factory: :currency)
     output_amount { 1.5 }
-    output_currency { nil }
+    association(:output_currency, factory: :currency)
     transaction_date { "2022-09-17 01:26:18" }
   end
 end
