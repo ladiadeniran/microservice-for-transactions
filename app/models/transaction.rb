651852166c5ea1a6be3ba4e1299d6105765f178b
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 03:20:55 +0100

    Fix foreign key reference to the output_currency_id

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,7 @@
 class Transaction < ApplicationRecord
   belongs_to :customer
   belongs_to :input_currency, foreign_key: :input_currency_id, class_name: 'Currency'
-  belongs_to :output_currency, foreign_key: :input_currency_id, class_name: 'Currency'
+  belongs_to :output_currency, foreign_key: :output_currency_id, class_name: 'Currency'
 
   validates :customer_id, :input_currency_id, :input_amount, :output_amount, :output_currency_id, :transaction_date, presence: true
 end
