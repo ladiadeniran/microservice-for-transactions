6b2c98424f77fb7f70e99bf578dbebb972f68b11
ladiadeniran <oladipupo.adeniran@gmail.com>
2022-09-17 04:50:55 +0100

    Add api error handler to have a single source of handling errors

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -2,4 +2,26 @@
 
 module ErrorHandler
   extend ActiveSupport::Concern
+
+  module ErrorHandler
+  extend ActiveSupport::Concern
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |error|
+      invalid_request(error.to_s)
+    end
+
+    rescue_from NoMethodError do |error|
+      invalid_request(error.to_s)
+    end
+
+    rescue_from ActionController::ParameterMissing do |error|
+      invalid_request(error.to_s)
+    end
+
+    def invalid_request(error_message, status: :bad_request)
+      render json: error_message.to_json, status: status
+    end
+  end
+end
+
 end
